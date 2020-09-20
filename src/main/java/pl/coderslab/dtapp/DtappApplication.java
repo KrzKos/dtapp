e0c2c179d29d6ef022d1a35a55e8910272e0787d
Krzysiek Kostkiewicz <krzysztof.kostkiewicz@gmail.com>
2020-09-20 12:36:11 +0200

    init

---
+++ src/main/java/pl/coderslab/dtapp/DtappApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.dtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/dtapp/DtappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.dtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
